d30a7013ff02b5f43d6eb0e8b1df430488b7a26c
dylandray <120407558+dylandray@users.noreply.github.com>
2024-03-04 18:37:25 +0100

    Update simpleOnionRouter.ts

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -3,20 +3,41 @@ import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 
 export async function simpleOnionRouter(nodeId: number) {
+
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
-  // TODO implement the status route
-  // onionRouter.get("/status", (req, res) => {});
+  const lastReceivedMessage = null;
+  const lastReceivedDecryptedMessage = null;
+  const lastMessageDestination = null;
+  
+  onionRouter.get("/status/", (req, res) => {
+    res.send("live");
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
+
     console.log(
       `Onion router ${nodeId} is listening on port ${
         BASE_ONION_ROUTER_PORT + nodeId
       }`
+
     );
   });
 
   return server;
+  
 }
