96a576d64dcdae75f032d2175ff0b9e279ec2eec
dylandray <120407558+dylandray@users.noreply.github.com>
2024-03-12 14:42:22 +0100

    Add files via upload

---
+++ users/user.ts
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { BASE_USER_PORT } from "../config";
+
+type Body = {
+  message: string;
+};
+
+export async function user(userId: number) {
+  const _user = express();
+  
+  _user.use(express.json());
+  _user.use(bodyParser.json());
+  
+  let lastReceivedMessage: string | null = null;
+  let lastSentMessage: string | null = null;
+
+  _user.get("/status", (req, res) => {
+    res.send("live");
+  });
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
+  });
+
+  _user.post("/message", (req: Request<{}, {}, Body>, res: Response) => {
+    const { message } = req.body;
+    lastReceivedMessage = message;
+    res.status(200).send("success");
+  });
+
+  const server = _user.listen(BASE_USER_PORT + userId, () => {
+    console.log(`User ${userId} listening on port ${BASE_USER_PORT + userId}`);
+  });
+
+  return server;
+}
